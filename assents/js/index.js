32c4c90d7060882a1bbc7345b74911a767b35c12
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-08-17 11:04:08 -0300

    Erro de salvamento 2.0 corrigido

--- assents/js/index.js
+++ assents/js/index.js
@@ -1,70 +1,48 @@
-(function () {
-  const marca = document.querySelector("#marca");
-  const modelo = document.querySelector("#modelo");
-  const anoDeFabricacao = document.querySelector("#anoDeFabricaçao");
-  const btn = document.querySelector("#btn");
-  const arrObjetos = []; //Array para guardar os objetos
-
-  function getValue(marca, modelo, anoDeFabricacao) {
-    return {
-      marca,
-      modelo,
-      anoDeFabricacao,
-    };
-
-    // Vai Criar um  objeto com as informações acima
-    // Função de fábrica.
+(function(){
+  const marca = document.querySelector('#marca');
+  const modelo = document.querySelector('#modelo');
+  const anoDeFabricacao = document.querySelector('#anoDeFabricaçao');
+  const btn = document.querySelector('#btn');
+  const arrObjetos = [];//Array para guardar os objetos
+  
+  function getValue(marca,modelo,anoDeFabricacao){
+      return {
+         marca,
+         modelo,
+         anoDeFabricacao
+      };
+      // Vai Criar um  objeto com as informações acima
+      // Função de fábrica.
   }
-
-  function limpaImputs() {
-    marca.value = "";
-    modelo.value = "";
-    anoDeFabricacao.value = "";
-    marca.focus();
-    // Vai limpar os inputs e reidirecionar para o primeiro.
+  function limpaImputs(){
+      marca.value = "";
+      modelo.value = "";
+      anoDeFabricacao.value = "";
+      marca.focus();
+      // Vai limpar os inputs e redirecionar para o primeiro.
   }
-  document.addEventListener("keyup", (e) => {
-    if (e.keyCode === 13) {
-      if (
-        marca.value === "" ||
-        modelo.value === "" ||
-        anoDeFabricacao.value === ""
-      ) {
-        alert("Preencha todos as informções corretamente!");
-        return;
-      }
-      arrayObj();
-      limpaImputs();
-      salvarVeiculos();
-      alert("Veiculo cadastrado com sucesso!");
-    }
-  });
-
-  btn.addEventListener("click", () => {
-    if (
-      marca.value === "" ||
-      modelo.value === "" ||
-      anoDeFabricacao.value === ""
-    ) {
-      alert("Preencha todos as informções corretamente!");
-      return;
-    }
-    arrayObj();
-    limpaImputs();
-    salvarVeiculos();
-    alert("Veiculo cadastrado com sucesso!");
-    // Chamando as funções com o click do butão
-  });
-
-  function arrayObj() {
-    arrObjetos.push(getValue(marca.value, modelo.value, anoDeFabricacao.value));
+  btn.addEventListener('click',()=>{
+       if(marca.value === "" || modelo.value=== "" || anoDeFabricacao.value === ""){
+          alert('ERROR')
+          return;
+       }
+       arrayObj();
+       limpaImputs();
+       salvarVeiculos();
+       alert('Veiculo cadastrado com sucesso!')
+      // Chamando as funções com o click do butão
+  })
+  
+  function arrayObj(){
+   arrObjetos.push(getValue(marca.value,modelo.value,anoDeFabricacao.value));
     return arrObjetos;
-    //  Adicionar o objeto criado dentro do array
+  //  Adicionar o objeto criado dentro do array
   }
-
-  function salvarVeiculos() {
-    localStorage.setItem(`veiculos`, JSON.stringify(arrayObj()));
-    // Salvar os objetos do array criado no localStorage
+  
+  function salvarVeiculos(){
+      localStorage.setItem(`veiculos`,JSON.stringify(arrObjetos));
+  // Salvar os objetos do array criado no localStorage
   }
+  
 })();
   

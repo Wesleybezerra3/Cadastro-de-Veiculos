9f12d810e890ad0ecccc91115b02f9b7e29d1894
Wesleybezerra3 <wesleybezerra770@gmail.com>
2023-08-20 13:53:14 -0300

    estilo da pagina melhorado, sistema de delete melhorado , sistema de exibição melhorado

--- assents/js/exibir.js
+++ assents/js/exibir.js
@@ -4,6 +4,7 @@
   document.addEventListener("DOMContentLoaded", function () {
     exibirVeiculos();
     apagarVeiculo();
+    addText();
   }); // Carrega as funções
 
   const getLocalStorage = () => {
@@ -11,67 +12,123 @@
     return JSON.parse(veiculos) || [];
   }; //Pegar os veiculos cadastrados no localStorage
 
+  function addText(){ //Vai adicionar um aviso se o estoque estiver vazio.
+    const text = document.querySelector('#aviso');
+    if(getLocalStorage().length === 0){
+      text.innerText = 'Sem veiculos no estoque!'
+    }else{
+      text.innerText = ''
+    }
+    
+  }
+  const icon = (lixeira, car) => {
+    const divIcon = document.createElement("div");
+    divIcon.classList.add("divIcon");
+    divIcon.appendChild(lixeira);
+    divIcon.appendChild(car);
+    return divIcon;
+  };
+
   const criarLixeira = () => {
     const lixeira = document.createElement("i");
     lixeira.classList.add("fa-solid", "fa-trash", "apagar");
     return lixeira;
-  };// Função para criar um botão de apagar
-  
+  }; // Função para criar um botão de apagar.
 
-  function apagarVeiculo() { //Função para apagar o veiculo
+  const veiculoVendido = () => {
+    // Função para criar um botão de 'veiculo vendido'.
+    const car = document.createElement("i");
+    car.classList.add("fa-solid", "fa-car");
+    return car;
+  };
 
+  function apagarVeiculo() {
     areaDeCadastrados.addEventListener("click", (e) => {
       const el = e.target;
+
       if (el.classList.contains("apagar")) {
         const confirmar = confirm(
-          "Se apaga não será possivel recuperar o veiculo excluido, Deseja continuar?"
+          "Se apagar, não será possível recuperar o veículo excluído. Deseja continuar?"
         );
+
         if (confirmar) {
           const veiculos = getLocalStorage();
-          const index = Array.from(
-            el.parentElement.parentElement.children
-          ).indexOf(el.parentElement);
+          const articleElement = el.closest(".veiculos"); // Encontra o elemento <article> pai do botão
 
-          if (index !== -1) {
-            veiculos.splice(index, 1);
-            localStorage.setItem("veiculos", JSON.stringify(veiculos));
-            el.parentElement.remove();
+          if (articleElement) {
+            const index = Array.from(
+              articleElement.parentElement.children
+            ).indexOf(articleElement);
+
+            if (index !== -1) {
+              veiculos.splice(index, 1);
+              localStorage.setItem("veiculos", JSON.stringify(veiculos));
+              articleElement.remove();
+            }
           }
         }
       }
     });
-  } 
+  }
 
   // Função para criar e adicionar uma div com os elementos
   function article(elementos) {
     const article = document.createElement("article");
-    article.classList.add("veiculos");
+    article.classList.add("vehicleDetail");
     elementos.forEach((elemento) => {
       article.appendChild(elemento);
     });
     areaDeCadastrados.appendChild(article);
   }
 
+
   function exibirVeiculos() {
     areaDeCadastrados.innerHTML = ""; // Limpa o conteúdo antes de recriar
     const veiculosLista = getLocalStorage();
 
     veiculosLista.forEach((veiculo) => {
-      const h1 = document.createElement("h1");
-      h1.innerText = veiculo.marca;
-
-      const pMarca = document.createElement("p");
-      pMarca.innerHTML = `<span>MARCA:</span> ${veiculo.marca}`;
-
-      const pModelo = document.createElement("p");
-      pModelo.innerHTML = `<span>MODELO:</span> ${veiculo.modelo}`;
-
+     
       const pAno = document.createElement("p");
       pAno.innerHTML = `<span>ANO DE FABRICAÇÃO:</span> ${veiculo.anoDeFabricacao}`;
 
-      const apagar = criarLixeira();
+      const pKm = document.createElement("p");
+      pKm.innerHTML = `<span>KM:</span> ${veiculo.km}`;
 
-      article([h1, pMarca, pModelo, pAno, apagar]);
+      const pValor = document.createElement("p");
+      pValor.innerHTML = `<span>VALOR:</span> ${veiculo.valor}`;
+
+      const divIcon = icon(criarLixeira(), veiculoVendido());
+
+      function divModelo(veiculo){
+        const h1 = document.createElement("h1");
+        const div = document.createElement('div');
+        div.classList.add('vehicleDetailHeader');
+        const p = document.createElement('p');
+          
+        const modelo = veiculo.modelo.split(' ');
+
+        h1.innerHTML = `${veiculo.marca} <span>${modelo[0]}</span>`;
+        p.innerText = modelo.slice(1).join(' ');
+
+        const children = [
+          h1,
+          p
+        ];
+
+        children.forEach(child=>{
+         div.appendChild(child);
+        });
+       
+        return div;
+      }
+
+     
+
+      article([divModelo(veiculo), pAno, pKm,pValor,divIcon]);
+
     });
   }
+
+
+
 })();
--- assents/js/index.js
+++ assents/js/index.js
@@ -5,13 +5,17 @@
   const marca = document.querySelector("#marca");
   const modelo = document.querySelector("#modelo");
   const anoDeFabricacao = document.querySelector("#anoDeFabricacao");
+  const km = document.querySelector('#km');
+  const valor = document.querySelector('#valor');
   const btn = document.querySelector("#btn");
 
   btn.addEventListener("click", () => {
     if (
       marca.value === "" ||
       modelo.value === "" ||
-      anoDeFabricacao.value === ""
+      anoDeFabricacao.value === "" ||
+      km.value === "" ||
+      valor.value === ""
     ) {
       alert("Preencha os campos da maneira correta!");
       return;
@@ -20,16 +24,20 @@
     clearInputs();
     alert('Veiculo cadastrado com sucesso!')
   });
-  function Cadastro(marca, modelo, anoDeFabricacao) {
+  function Cadastro(marca, modelo, anoDeFabricacao,km,valor) {
     this.marca = marca;
     this.modelo = modelo;
     this.anoDeFabricacao = anoDeFabricacao;
+    this.km = km;
+    this.valor = valor;
   } // Função construtora, Vai criar um objeto com os dados acima
 
   function clearInputs() {
     marca.value = "";
     modelo.value = "";
     anoDeFabricacao.value = "";
+    km.value = "";
+    valor.value = "";
 
     marca.focus();
     // Vai limpar os inputs e reidirecionar para o primeiro.
@@ -38,7 +46,9 @@
     const veiculos = new Cadastro(
       marca.value,
       modelo.value,
-      anoDeFabricacao.value
+      anoDeFabricacao.value,
+      km.value,
+      valor.value,
     );
     arr.push(veiculos);
     save();
